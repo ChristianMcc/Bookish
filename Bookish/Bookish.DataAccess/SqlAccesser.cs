b2e5f54a1bbd68ae0e6aa330e92f0c8bcda5be7a
Christian McCaffery <Christian.McCaffery@softwire.com>
2018-11-15 09:55:03 +0000

    Started creating addbook form and sql addbook method

--- Bookish/Bookish.DataAccess/SqlAccesser.cs
+++ Bookish/Bookish.DataAccess/SqlAccesser.cs
@@ -16,5 +16,29 @@ namespace Bookish.DataAccess
             var bookList = db.Query<Book>(sqlString).ToList();
             return bookList;
         }
+
+        public static List<Book> GetYourBooks(string userid)
+        {
+            IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["BookishConnectionString"].ConnectionString);
+            string sqlString1 = "SELECT * FROM TABLE WHERE ID IS "+ userid +" [BookId],[Author],[Title],[ISBN] FROM [Books]";
+            var bookIdList = db.Query<Book>(sqlString1).ToList();
+
+            IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["BookishConnectionString"].ConnectionString);
+            string sqlString2 = "SELECT TOP 100 [BookId],[Author],[Title],[ISBN] FROM [Books]";
+            var bookList = db.Query<Book>(sqlString2).ToList();
+            return bookList;
+        }
+
+        public void AddBookToLibrary(string author, string title, string ISBN)
+        {
+            IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["BookishConnectionString"].ConnectionString);
+            db.Open();
+            var book = new Book();
+            book.Title = title;
+            book.Author = author;
+            book.ISBN = ISBN;
+            string processQuery = "INSERT INTO BOOKS VALUES (@A, @B)";
+            db.Execute(processQuery, book);
+        }
     }
 }
